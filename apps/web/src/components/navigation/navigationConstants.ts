3d6336094df66e02019afda322d2268c000b4f94
Bettina Schlager <99039876+ovenmitt@users.noreply.github.com>
2025-06-05 13:03:21 -0400

    Fixing mismatch with icon lib (#42)

--- apps/web/src/components/navigation/navigationConstants.ts
+++ apps/web/src/components/navigation/navigationConstants.ts
@@ -1,22 +1,22 @@
-import { LogIn, LogOut, UserPlus } from 'lucide-react';
+import { IconLogin, IconLogout, IconUserPlus } from '@tabler/icons-react';
 
 import { NavigationItemType } from '@/components/navigation/NavigationItem';
 
-// Navigation items that are not user-related (always visible)
+// Navigation items that are userprofile independent
 export const generalNavigations: NavigationItemType[] = [
   { label: 'Models', href: '/models', variant: 'tertiary' },
   { label: 'Documentation', href: '/docs', variant: 'tertiary' },
 ];
 
-// User-related navigation items for signed-in users
+// Navigation items for signed-in users
 export const signedInUserNavigations: NavigationItemType[] = [
   { label: 'Dashboard', liquidOnly: true, href: '/dashboard', variant: 'tertiary' },
   { label: 'Profile', href: '/profile', variant: 'tertiary' },
-  { label: 'Sign Out', href: '#', action: 'sign-out', variant: 'tertiary', icon: LogOut },
+  { label: 'Sign Out', href: '#', action: 'sign-out', variant: 'tertiary', icon: IconLogout },
 ];
 
-// User-related navigation items for signed-out users
+// Navigation items for signed-out users
 export const signedOutUserNavigations: NavigationItemType[] = [
-  { label: 'Sign In', href: '/sign-in', variant: 'tertiary', icon: LogIn },
-  { label: 'Sign Up', href: '/sign-up', variant: 'tertiary', icon: UserPlus },
+  { label: 'Sign In', href: '/sign-in', variant: 'tertiary', icon: IconLogin },
+  { label: 'Sign Up', href: '/sign-up', variant: 'tertiary', icon: IconUserPlus },
 ];
