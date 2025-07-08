e65617cddfe97e53ff2aff24b6cbecd2276af6a7
Darian Bhathena <darian@liquid.ai>
2025-07-08 16:48:08 -0400

    Update documentation structure (#79)

---
+++ apps/web/src/content/android/_meta.ts
@@ -0,0 +1,8 @@
+import type { MetaRecord } from 'nextra';
+
+const meta: MetaRecord = {
+  'android-quick-start-guide': 'Quick Start',
+  'android-api-spec': 'API Reference',
+};
+
+export default meta;
---
+++ apps/web/src/content/ios/_meta.ts
@@ -0,0 +1,8 @@
+import type { MetaRecord } from 'nextra';
+
+const meta: MetaRecord = {
+  // 'ios-quick-start-guide': "Quick Start",
+  'ios-api-spec': 'API Reference',
+};
+
+export default meta;
