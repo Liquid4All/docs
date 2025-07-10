50238f679a615d3211b91cf56ebb8c83cf41b104
Bettina Schlager <99039876+ovenmitt@users.noreply.github.com>
2025-07-10 10:24:17 -0400

    Ovenmitt/finalizing landing page (#84)
    * Updated text content and added code language context to setup section
    * Added quickstart CTA and GitHub examples link
    * Changed hero tab default to Leap SDK and fixed tab ordering
    * Added Discord placeholder to footer with minor text adjustments

--- apps/web/src/components/Container.tsx
+++ apps/web/src/components/Container.tsx
@@ -6,7 +6,7 @@ export function Container({ className, ...props }: ComponentPropsWithoutRef<'div
   return (
     <div
       className={cn(
-        'sm:max-w-[800px] md:max-w-[1200px] xl:max-w-[1440px] 2xl:max-w-[1440px] 3xl:max-w-[1550px] px-4 md:px-6 xl:px-8 2xl:px-12 3xl:px-0 mx-auto',
+        'sm:max-w-[800px] md:max-w-[1200px] xl:max-w-[1440px] 2xl:max-w-[1440px] 3xl:max-w-[1550px] px-4 md:px-10 2xl:px-12 3xl:px-0 mx-auto',
         className
       )}
       {...props}
