b0346684f0a237dc9daf07fea51f0383d2638a53
Mengxiao Lin <mengxiao.lin@liquid.ai>
2025-08-11 18:09:00 -0700

    Add function call documents (#193)
    Bump versions of both Android and iOS SDKs to v0.4 and add documents on
    function calling.

--- apps/web/src/lib/sdk-versions.ts
+++ apps/web/src/lib/sdk-versions.ts
@@ -1,5 +1,5 @@
-export const LATEST_IOS_VERSION = 'v0.3.0';
-export const PREVIOUS_IOS_VERSION = 'v0.2.0';
+export const LATEST_IOS_VERSION = 'v0.4.0';
+export const PREVIOUS_IOS_VERSION = 'v0.3.0';
 export const OLDEST_IOS_VERSION = 'v0.1.0';
 
-export const IOS_VERSIONS = [LATEST_IOS_VERSION, PREVIOUS_IOS_VERSION, OLDEST_IOS_VERSION] as const;
+export const IOS_VERSIONS = ['v0.4.0', 'v0.3.0', 'v0.2.0', 'v0.1.0'] as const;
