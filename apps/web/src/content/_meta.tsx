ac60aec35a594f2ae59dc0feb1297f780589ef6e
Darian Bhathena <darian@liquid.ai>
2025-08-18 07:49:19 -0400

    Darian/lea 303 laptop support messaging (#220)

---
+++ apps/web/src/content/_meta.tsx
@@ -0,0 +1,32 @@
+import { IconAdjustments, IconCode } from '@tabler/icons-react';
+import type { MetaRecord } from 'nextra';
+
+const meta: MetaRecord = {
+  index: 'Introduction',
+  '-': {
+    type: 'separator',
+    title: (
+      <div className="flex flex-row gap-1 items-center">
+        <IconAdjustments className="w-5 h-5" /> Specialize
+      </div>
+    ),
+  },
+  finetuning: 'Finetuning',
+  '--': {
+    type: 'separator',
+    title: (
+      <div className="flex flex-row gap-1 items-center">
+        <IconCode className="w-5 h-5" /> Deploy
+      </div>
+    ),
+  },
+  'leap-bundle': { title: 'Model Bundling Service', theme: { collapsed: true } },
+  'edge-sdk': { title: 'Edge SDK', theme: { collapsed: true } },
+  'laptop-support': 'Laptop Support',
+  '---': {
+    type: 'separator',
+  },
+  changelog: 'Changelog',
+};
+
+export default meta;
